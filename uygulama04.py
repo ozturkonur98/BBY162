6e07ca0ced99b871a086fe32ac94fbdb04e76f9f
ozturkonur98 <48288305+ozturkonur98@users.noreply.github.com>
2019-04-24 14:47:40 +0300

    Update uygulama04.py

--- uygulama04.py
+++ uygulama04.py
@@ -26,13 +26,18 @@ print ("\n\t\t Soru 3 ")
 #3. Aşağıdaki sözlük öğesinde yer alan kelime alanında klavyeden girilen veri ile bir arama yaptırarak ilgili kelimeye karşılık gelen açıklamayı ve kelimenin kendisini ekrana yazdıracak python kodunu yazınız. Eğer klavyeden girilen kelime sözlükte yoksa uygun bir mesaj ile yeniden arama yapılabilmesi için gereken düzenlemeyi yapınız (5P).
 
 sozluk = {"Elma" : "Ağaçta yetişen bir tür meyve" , "Salatalık" : "Fidan üzerinde büyüyen bir tür sebze" }
-kgiris = input("Hangi sözcüğü aramak istiyorsunuz? Elma/Salatalık : ")
 
-if kgiris == "Elma":
-    kgiris = kgiris.swapcase()
-    print(sozluk["Elma"])
-elif kgiris == "Salatalık":
-    kgiris == kgiris.swapcase()
-    print(sozluk["Salatalık"])
-else:
-    print("Aradığınız kelime sözlükte bulunamamaktadır. Lütfen Tekrar arama yapınız.")
+def ara():
+
+    kgiris = input("Hangi sözcüğü aramak istiyorsunuz? Elma/Salatalık : ")
+
+    if kgiris == "Elma":
+        kgiris = kgiris.swapcase()
+        print(sozluk["Elma"])
+    elif kgiris == "Salatalık":
+        kgiris == kgiris.swapcase()
+        print(sozluk["Salatalık"])
+    else:
+        print("Aradığınız kelime sözlükte bulunamamaktadır. Lütfen Tekrar arama yapınız.")
+        ara()
+ara()
